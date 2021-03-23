95cfa4c3f2e3ba62f1e61be9813785950f249917
paynej5504 <54283576+paynej5504@users.noreply.github.com>
2021-03-23 12:30:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
+#imports packages
 from setuptools import setup, find_packages
 requires = [
 'flask',
 'url_for'
 ]
 setup(
-name='flask_first_app',
-version='0.0',
-description='My first Python web app built with Flask',
-author='<Your actual name here>',
+name='flask_first_app', #name of the app
+version='0.0', #app version
+description='My first Python web app built with Flask', #description of app
+author='<Your actual name here>', #author's name
 author_email='<Your actual e-mail address here>',
 keywords='web flask',
 packages=find_packages(),
